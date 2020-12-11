9d695bcd476e3f03abf8908064c48186fba318d6
lindashuai <1196917967@qq.com>
2020-12-11 18:05:49 +0800

    feat:label for text

--- LDSFaceDistanceDemo/ViewController.m
+++ LDSFaceDistanceDemo/ViewController.m
@@ -9,13 +9,27 @@
 #import <LDSFaceDistance/LDSFDDefine.h>
 #import <LDSFaceDistance/LDSFDFaceToCameraDistance.h>
 
+@interface ViewController ()
+
+@property(nonatomic, strong) UILabel *label;
+
+@end
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view addSubview:self.label];
     // Privacy - Camera Usage Description ->info.plist
+    __weak typeof(self) weakSelf = self;
     [LDSFDFaceToCameraDistance startWithConfiguration:[self createFDConfig] detectBlock:^(BOOL success, NSUInteger distance) {
         NSLog(@"distance %ld", distance);
+        weakSelf.label.text = [NSString stringWithFormat:@"face to camera distance %ld", distance];
+        if(distance < 1000) {
+            //weakSelf.label.hidden = NO;
+        } else {
+            //weakSelf.label.hidden = YES;
+        }
     }];
 }
 
@@ -25,6 +39,17 @@
     config.firstRepeatDelay = 5.0;
     config.timeInterval = 2.0;
     return config;
+}
+
+- (UILabel *)label {
+    if(_label == nil) {
+        _label = [[UILabel alloc]initWithFrame:CGRectMake(0, (self.view.frame.size.height / 2) - 100, self.view.frame.size.width, 100)];
+        _label.font = [UIFont systemFontOfSize:20];
+        _label.textColor = [UIColor blackColor];
+        _label.textAlignment = NSTextAlignmentCenter;
+        //_label.hidden = YES;
+    }
+    return _label;
 }
 
 @end
